74fb4bd4e5d4876cae0d2b55f1dbf3ce7d781798
Eugene Kushnir <kushnir.fort@gmail.com>
2022-08-17 19:10:18 +0300

    trello-fix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const header__navbar = document.querySelector('.header__navbar');
 const header = document.querySelector('.header');
 const header__login = document.querySelector('.header__login');
 const header__logo = document.querySelector('.header__logo');
+const logo_wrapper = document.querySelector('.logo_wrapper');
 const competitions_card = document.querySelectorAll('.competitions-card');
 const competitions_cards = document.querySelector('.competitions-cards');
 const swiper = document.querySelector('.swiper');
@@ -16,6 +17,7 @@ burger.addEventListener('click', ()=>{
    header__navbar.classList.toggle('_active');
    header__login.classList.toggle('_active');
    header__logo.classList.toggle('active');
+   logo_wrapper.classList.toggle('active');
    burger.classList.toggle('active');
    header.classList.toggle('active');
 
@@ -26,6 +28,7 @@ burger.addEventListener('click', ()=>{
             header__navbar.classList.remove('_active');
             header__login.classList.remove('_active');
             header__logo.classList.remove('active');
+            logo_wrapper.classList.remove('active');
             header.classList.remove('active');
             burger.classList.remove('active');
          }
